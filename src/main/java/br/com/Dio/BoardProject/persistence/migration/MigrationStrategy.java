f486c0170d0dc2b5fa203195abdaabe67886676e
Vitor Vasconcelos <vitorvasconcelos_@hotmail.com>
2025-08-12 11:26:48 -0300

    incluindo todos os arquivos de configurações

---
+++ src/main/java/br/com/Dio/BoardProject/persistence/migration/MigrationStrategy.java
@@ -0,0 +1,41 @@
+package br.com.Dio.BoardProject.persistence.migration;
+
+
+import lombok.AllArgsConstructor;
+
+import java.io.FileOutputStream;
+import java.sql.Connection;
+
+@AllArgsConstructor
+public class MigrationStrategy {
+
+    private final Connection connection;
+
+    public void executeMigration(){
+        var originalOut = System.out;
+        var originalErr = System.err;
+        try(var fos = new FileOutputStream("liquibase.log")){
+            System.setOut(new PrintStream(fos));
+            System.setErr(new PrintStream(fos));
+            try(
+                    var connection = getConnection();
+                    var jdbcConnection = new JdbcConnection(connection);
+            ){
+                var liquibase = new Liquibase(
+                        "/db/changelog/db.changelog-master.yml",
+                        new ClassLoaderResourceAccessor(),
+                        jdbcConnection);
+                liquibase.update();
+            } catch (SQLException | LiquibaseException e) {
+                e.printStackTrace();
+                System.setErr(originalErr);
+            }
+        } catch (IOException ex){
+            ex.printStackTrace();
+        } finally {
+            System.setOut(originalOut);
+            System.setErr(originalErr);
+        }
+    }
+
+}
